91fcec04ef443ffadf3371d898aa019e28e97426
guyd@rpi6 <guydvir.tech@gmail.com>
2018-08-12 18:42:33 +0300

    update getip- to retry connection. with loop and try except lines

--- getip.py
+++ getip.py
@@ -1,12 +1,20 @@
 import socket
 import urllib.request
+import time
 
 
 def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    internal_ip = s.getsockname()[0]
-    s.close()
-    external_ip = urllib.request.urlopen('http://ident.me').read().decode('utf8')
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(("8.8.8.8", 80))
+            internal_ip = s.getsockname()[0]
+            s.close()
+            external_ip = urllib.request.urlopen('http://ident.me').read().decode('utf8')
+            return internal_ip, external_ip
 
-    return internal_ip, external_ip
+        except OSError:
+            #internal_ip ='0.0.0.0'
+            #external_ip = '0.0.0.0'
+            time.sleep(5)
+  
